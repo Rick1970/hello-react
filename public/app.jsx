02c2752d1d9a558d24938bea480d84fff285c58c
Rick Thornbrugh <Rick Thornbrugh>
2017-01-10 15:44:14 -0800

    initial react app.

---
+++ public/app.jsx
@@ -0,0 +1,36 @@
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is a message from a component.'
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+    alert(name);
+  },
+  render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
+    return(
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type='text' ref='name' />
+          <button>Set Name</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Rick';
+var newMessage = 'This is not the default message from the component.';
+ReactDOM.render(
+  <Greeter name={firstName} message={newMessage}/>,
+  document.getElementById('app')
+);
